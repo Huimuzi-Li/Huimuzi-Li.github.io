a6131d3ff1fce9fb1323f8e671fc6d5e95b3b63a
Huimuzi-Li <hlii0109@student.monash.edu>
2023-05-30 14:56:25 +1000

    new

---
+++ demo/interaction.js
@@ -0,0 +1,90 @@
+
+$(document).ready(function() {
+
+    $("div.1").click(function () {
+        $("div.welcome").addClass("show");
+        $("div.welcome").click(function () {
+        $(this).removeClass("show");
+        });
+    });
+
+    $("div.2").click(function () {
+        $("div.p2").addClass("show");
+        $("div.p2").click(function () {
+        $(this).removeClass("show");
+        });
+    });
+
+    $("div.3").click(function () {
+        $("div.p3").addClass("show");
+        $("div.p3").click(function () {
+        $(this).removeClass("show");
+        });
+    });
+
+    $("div.4").click(function () {
+        $("div.p4").addClass("show");
+        $("div.p4").click(function () {
+        $(this).removeClass("show");
+        });
+    });
+    
+    $("div.5").click(function () {
+        $("div.p5").addClass("show");
+        $("div.p5").click(function () {
+        $(this).removeClass("show");
+        });
+    });
+   
+    $("div.6").click(function () {
+        $("div.p6").addClass("show");
+        $("div.p6").click(function () {
+        $(this).removeClass("show");
+        });
+    });  
+    
+    $("div.7").click(function () {
+        $("div.p7").addClass("show");
+        $("div.p7").click(function () {
+        $(this).removeClass("show");
+        });
+    });   
+
+    $("div.8").click(function () {
+        $("div.p8").addClass("show");
+        $("div.p8").click(function () {
+        $(this).removeClass("show");
+        });
+    });   
+
+    $("div.9").click(function () {
+        $("div.p9").addClass("show");
+        $("div.p9").click(function () {
+        $(this).removeClass("show");
+        });
+    });   
+
+    $("div.10").click(function () {
+        $("div.p10").addClass("show");
+        $("div.p10").click(function () {
+        $(this).removeClass("show");
+        });
+    });   
+
+    $("div.11").click(function () {
+        $("div.p11").addClass("show");
+        $("div.p11").click(function () {
+        $(this).removeClass("show");
+        });
+    });   
+
+    $("div.12").click(function () {
+        $("div.p12").addClass("show");
+        $("div.p12").click(function () {
+        $(this).removeClass("show");
+        });
+    });   
+
+
+
+});
